3ff861e26b0df08a0bc240feb22db75271f2b3ec
Viola Arrfors <vioarr@vioarr2022.gbg.intranet>
2024-09-17 13:59:27 +0200

    uppdaterad lite

---
+++ ravi-script.js
@@ -0,0 +1,5 @@
+document.getElementById("ClickMeBitch").onclick
+    = () => {
+        alert("Clicked that bitch")
+        //* Alert blir en sån ful liten lapp som fälls ut där uppe. Den är typ rätt tråkig. */
+    }
