716adf1b069768e7e9fb6dffd5e1ffd42b0700b4
Viola Arrfors <vioarr@vioarr2022.gbg.intranet>
2024-09-17 15:54:25 +0200

    js experiment, vibe button

--- ravi-script.js
+++ ravi-script.js
@@ -3,3 +3,17 @@ document.getElementById("ClickMeBitch").onclick
         alert("Clicked that bitch")
         //* Alert blir en sån ful liten lapp som fälls ut där uppe. Den är typ rätt tråkig. */
     }
+
+function vibePrompt() {
+    let result = window.prompt("what's the vibe", "vibe");
+    let text;
+
+    if (result === null) {
+        text = "oki skit i d då"
+    } else {
+        text = "oki fett, " + result + ', bra vibe'
+    }
+
+    let resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = text;
+}
